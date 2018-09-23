beb97b52a218404834062b6132de6611b68559cd
Jean Mariano <jeandmariano@gmail.com>
2018-09-23 18:39:16 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Moon from "./Moon.js";
+import BoatBackground from "./BoatBackground";
+import BoatForeground from "./BoatForeground";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="sky">
+          <h1 className="title">jean mariano</h1>
+          <div className="moonshadow" />
+          <Moon />
+
+          <div className="star star-1" />
+          <div className="star star-2" />
+          <div className="star star-3" />
+          <div className="star star-4" />
+          <div className="stars small" />
+          <div className="stars medium" />
+          <div className="stars large" />
+        </div>
+
+        <div className="water">
+          <div className="wave wave1" />
+          <div className="wave wave2" />
+          <div className="talkbubble">Welcome to my website.</div>
+          <div className="Boat">
+            <div className="BoatBackground">
+              <BoatBackground />
+            </div>
+            <div className="ladder">
+              <div className="side side1" />
+              <div className="rung rung1" />
+              <div className="rung rung2" />
+              <div className="rung rung3" />
+              <div className="rung rung4" />
+              <div className="rung rung5" />
+              <div className="rung rung6" />
+              <div className="rung rung7" />
+              <div className="rung rung8" />
+              <div className="rung rung9" />
+              <div className="rung rung10" />
+              <div className="side side2" />
+            </div>
+            <img
+              className="shibe"
+              src={`${process.env.PUBLIC_URL}/shibe.png`}
+            />
+            <div className="BoatForeground">
+              <BoatForeground />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/BoatBackground.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function BoatBackground() {
+  return (
+    <svg
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      viewBox="136.4 299.1 250.4 147.7"
+    >
+      <defs>
+        <path
+          id="a"
+          d="M285.4 338.6l-38-12.2c-12.2-4-24.7-7-37.3-9-2.4-.3-8.5-1.2-18.1-2.8l-5.6.5a75.8 75.8 0 0 0-46.4 21l-1.8 1.8 15.4 4.9c23.5 7.4 48.4 9.3 72.7 5.3l59-9.5z"
+        />
+      </defs>
+      <use fill="#bd734c" xlinkHref="#a" />
+      <use fill-opacity="0" stroke="#bfffff" stroke-width="0" xlinkHref="#a" />
+    </svg>
+  );
+}
+
+export default BoatBackground;
