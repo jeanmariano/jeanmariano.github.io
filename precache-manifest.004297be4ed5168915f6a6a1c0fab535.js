f0ff763b1254dd5eb36f82887ea99b63ccb3e3fb
Jean Mariano <jeandmariano@gmail.com>
2019-02-15 18:02:27 -0500

    Updates

---
+++ precache-manifest.004297be4ed5168915f6a6a1c0fab535.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "59ab6d50b67c16e6ba51",
+    "url": "/static/js/main.59ab6d50.chunk.js"
+  },
+  {
+    "revision": "b2f5ab1217bac76f1d55",
+    "url": "/static/js/2.b2f5ab12.chunk.js"
+  },
+  {
+    "revision": "59ab6d50b67c16e6ba51",
+    "url": "/static/css/main.7dbbed43.chunk.css"
+  },
+  {
+    "revision": "1bb786f9c85a6abc7658985ea0d04ebe",
+    "url": "/index.html"
+  }
+];
